26ca0e889dbf2e1ec75ff72ba15cf2b2da01fdf7
Shahbaz Husain Khan <khanshahbazhusain@gmail.com>
2025-01-15 21:55:56 +0530

    build sign -in page

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,49 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import images from '@/constants/images'
+import icons from '@/constants/icons'
 
 const SignIn = () => {
+
+  const handleLogin = () => {
+    console.log('Login')
+  }
+  
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
+    <SafeAreaView className='bg-white h-full'>
+      <ScrollView contentContainerClassName='h-full'>
+        <Image
+        className='w-full h-4/6'
+        resizeMode='contain'
+        source={images.onboarding}
+         />
+         <View className='px-10'>
+          <Text className='text-base text-center uppercase font-rubik text-black-200'>Welcome to ReState</Text>
+          <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'>Let's Get You Closer to {"\n"}
+            <Text className='text-primary-300'>Your Ideal Home</Text>
+          </Text>
+          <Text className='text-lg font-rubik text-black-200 text-center mt-12'>
+            Login to ReState with Google
+          </Text>
+          <TouchableOpacity onPress={handleLogin}
+          className='bg-white shadow-md shadow-zinc-300 rounded-full py-4 mt-5'
+          >
+            <View className='flex flex-row justify-center items-center'>
+            <Image
+            source={icons.google}
+            className='w-5 h-5'
+            resizeMode='contain'
+            />
+            <Text
+            className='text-lg font-rubik-medium text-black-300 ml-2'
+            >Continue with Google</Text>
+
+            </View>
+          </TouchableOpacity>
+         </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
---
+++ image.d.ts
@@ -0,0 +1,21 @@
+declare module '*.png'{
+    const value: any;
+    export default value;
+}
+
+declare module '*.jpg'{
+    const value: any;
+    export default value;
+}
+declare module '*.jpeg'{
+    const value: any;
+    export default value;
+}
+declare module '*.git'{
+    const value: any;
+    export default value;
+}
+declare module '*.svg'{
+    const value: any;
+    export default value;
+}
